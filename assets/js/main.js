17793c7e9a7cb6db5bb6d67dcfb7bdaded92953d
nardy70 <nardy70@gmail.com>
2018-01-21 02:02:11 +0430

    calendars

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,7 @@
 
 (function($) {
 
-	skel
-		.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
-
+	// Random background image
 	var images = [];
 	var nr_img = 7;
 
@@ -28,6 +20,112 @@
 		'background-attachment': 'fixed',
 		'background-size': 'cover'
 	});
+
+	// ------------ Start Events --------------------
+	var sampleEvents = {
+	"monthly": [
+		{
+		"id": 1,
+		"name": "Whole month event",
+		"startdate": "2018-01-01",
+		"enddate": "2018-01-31",
+		"starttime": "12:00",
+		"endtime": "2:00",
+		"color": "#99CCCC",
+		"url": ""
+		},
+		{
+		"id": 2,
+		"name": "Test encompasses month",
+		"startdate": "2018-01-29",
+		"enddate": "2018-02-02",
+		"starttime": "12:00",
+		"endtime": "2:00",
+		"color": "#CC99CC",
+		"url": ""
+		},
+		{
+		"id": 3,
+		"name": "Test single day",
+		"startdate": "2018-2-04",
+		"enddate": "",
+		"starttime": "",
+		"endtime": "",
+		"color": "#666699",
+		"url": "https://www.google.com/"
+		},
+		{
+		"id": 8,
+		"name": "Test single day",
+		"startdate": "2018-03-05",
+		"enddate": "",
+		"starttime": "",
+		"endtime": "",
+		"color": "#666699",
+		"url": "https://www.google.com/"
+		},
+		{
+		"id": 4,
+		"name": "Test single day with time",
+		"startdate": "2018-01-07",
+		"enddate": "",
+		"starttime": "12:00",
+		"endtime": "02:00",
+		"color": "#996666",
+		"url": ""
+		},
+		{
+		"id": 5,
+		"name": "Test splits month",
+		"startdate": "2018-01-25",
+		"enddate": "2016-12-04",
+		"starttime": "",
+		"endtime": "",
+		"color": "#999999",
+		"url": ""
+		},
+		{
+		"id": 6,
+		"name": "Test events on same day",
+		"startdate": "2018-01-25",
+		"enddate": "",
+		"starttime": "",
+		"endtime": "",
+		"color": "#99CC99",
+		"url": ""
+		},
+		{
+		"id": 7,
+		"name": "Test events on same day",
+		"startdate": "2018-01-25",
+		"enddate": "",
+		"starttime": "",
+		"endtime": "",
+		"color": "#669966",
+		"url": ""
+		},
+		{
+		"id": 9,
+		"name": "Test events on same day",
+		"startdate": "2018-01-25",
+		"enddate": "",
+		"starttime": "",
+		"endtime": "",
+		"color": "#999966",
+		"url": ""
+		}
+	]
+	};
+
+	$(window).load( function() {
+	    $('#mycalendar').monthly({
+			mode: 'event',
+			dataType: 'json',
+			events: sampleEvents
+		});
+	});
+	// -------- Finish Evens ----------------------------
+
 
 	$(function() {
 
---
+++ events.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Eventi
+description: Segui tutti i nostri spettacoli.
+---
+
+<div class="monthly" id="mycalendar"></div>
